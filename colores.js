78fc8f8dcf5cb965930ade473648411e2fcc52e8
EC2 Default User <ec2-user@ip-10-0-132-168.shared.cloud.transbank.local>
2019-05-28 21:01:28 +0000

    first commit

---
+++ colores.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+
+let colores = [
+    "rojo",
+    "azul",
+    "verde",
+    "cafe",
+    "pardo",
+    "celeste"
+];
+
+app.get('/', function (req, res) {
+    respuesta = {
+        error: true,
+        codigo: 200,
+        cotenido: 'home'
+    };
+    res.send('home');
+});
+
+app.get('/colores', function (req, res) {
+    res.send(colores);
+});
+
+app.use(function (req, res, next) {
+    res.status(404).send('URL no encontrada');
+});
+
+app.listen(3000, () => {
+    console.log("El servidor está inicializado en el puerto 3000");
+});
---
+++ nombres.js
@@ -0,0 +1,40 @@
+t express = require("express");
+const app = express();
+
+let nombres = [
+    "Héctor",
+    "Javier",
+    "Santos",
+    "Marco",
+    "Cayetano",
+    "Rodrigo",
+    "Francisco",
+    "Flavio",
+    "Juan",
+    "Andoni",
+    "Martín",
+    "Mateo",
+    "Pedro",
+    "Unai",
+];
+
+app.get('/', function (req, res) {
+    respuesta = {
+        error: true,
+        codigo: 200,
+        cotenido: 'home'
+    };
+    res.send('home');
+});
+
+app.get('/nombres', function (req, res) {
+    res.send(nombres);
+});
+
+app.use(function (req, res, next) {
+    res.status(404).send('URL no encontrada');
+});
+
+app.listen(3000, () => {
+    console.log("El servidor está inicializado en el puerto 3000");
+});
